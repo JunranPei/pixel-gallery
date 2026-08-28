8b1038dc6194f18180cea4fa700fc5a26a94fd1a
Junran_Pei <peijunranzhuofan@gmail.com>
2026-08-28 15:16:04 +0800

    chore(release): bump main to 4.3.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,8 +85,8 @@ android {
         )
         minSdk = 26
         targetSdk = 35
-        versionCode = 2046
-        versionName = "4.3.3.2-main"
+        versionCode = 2047
+        versionName = "4.3.3.3-main"
     }
 
     signingConfigs {
